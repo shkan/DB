0c47d74a7b6e71a783c581bb5be021199e82792f
prs1378 <prs1378@gmail.com>
2016-03-15 20:58:38 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -245,7 +245,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {our_id},
+    sudo_users = {152065669,87160007,105831687},
     admin_users = {},
     disabled_channels = {}
   }
